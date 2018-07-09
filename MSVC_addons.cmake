607771e441ee7842d0e1c099b6a069507c204d69
Sergey Fedorenko <serigfed@gmail.com>
2018-07-09 21:02:09 +0300

    Fixed the linking error with referencing to tbb-NOTFOUND.obj under MSVC.
    - Updated module FindTBB.cmake from https://github.com/justusc/FindTBB/pull/11/commits/2efbf64bbdc9a40f634fd3eb38084e5d9e8a9227.
    - Added the new definition into MSVC_addons.cmake.

--- MSVC_addons.cmake
+++ MSVC_addons.cmake
@@ -5,7 +5,7 @@ add_definitions("/MP")
 # Many targets overflow default size of object files with last packages, so we allow all our targets to have huge objs
 add_definitions("/bigobj")
 
-# to omit definition of min/max macros in the windows.h that disturbs to std::max/std::min expressions for msvc compiler
+# To omit definition of min/max macros in the windows.h that disturbs to std::max/std::min expressions for msvc compiler
 add_definitions("-DNOMINMAX")
 
 # Boost requires this because of removed std::unary_function since /std:c++14,
@@ -16,10 +16,18 @@ add_definitions("-D_HAS_AUTO_PTR_ETC=1")
 set(Boost_USE_STATIC_LIBS ON CACHE BOOL "" FORCE)
 
 if (NOT Boost_USE_STATIC_LIBS)
-	add_definitions("-DBOOST_SYSTEM_DYN_LINK")
-	add_definitions("-DBOOST_FILESYSTEM_DYN_LINK")
-	add_definitions("-DBOOST_PROGRAM_OPTIONS_DYN_LINK")
-#	add_definitions(${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
+    add_definitions("-DBOOST_SYSTEM_DYN_LINK")
+    add_definitions("-DBOOST_FILESYSTEM_DYN_LINK")
+    add_definitions("-DBOOST_PROGRAM_OPTIONS_DYN_LINK")
+#   add_definitions(${Boost_LIB_DIAGNOSTIC_DEFINITIONS})
 endif()
 
 set(gtest_force_shared_crt ON CACHE BOOL "" FORCE)
+
+# TBB library adds in two headers one of the tbb.lib or tbb_debug.lib depending on the _DEBUG definition.
+# But the problem is that the configuration RelWithDebInfo doesn't have _DEBUG and hence it's wrong.
+# Thus we turn off this feature in TBB by adding the definition below.
+#
+# Source: https://github.com/justusc/FindTBB/issues/8
+#
+add_definitions("-D__TBB_NO_IMPLICIT_LINKAGE")
