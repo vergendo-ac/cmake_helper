0adeef2a1f20c33e11c1345a9c3d15c558b10654
Andrew Penkrat <Andrey.Penkrat@lanit-tercom.com>
2019-08-01 13:44:53 +0300

    Add systemd::systemd cmake target

--- modules/FindSystemd.cmake
+++ modules/FindSystemd.cmake
@@ -4,16 +4,25 @@
 #  SYSTEMD_INCLUDE_DIRS - The systemd include directories
 #  SYSTEMD_LIBRARIES    - The libraries needed to use systemd
 
-include(FeatureSummary)
-set_package_properties(Systemd PROPERTIES
-        URL "http://freedesktop.org/wiki/Software/systemd/"
-        DESCRIPTION "System and Service Manager")
+if(NOT SYSTEMD_FOUND)
+  include(FeatureSummary)
+  set_package_properties(Systemd PROPERTIES
+          URL "http://freedesktop.org/wiki/Software/systemd/"
+          DESCRIPTION "System and Service Manager")
 
-find_package(PkgConfig QUIET)
-pkg_check_modules(PC_SYSTEMD QUIET libsystemd)
-find_library(SYSTEMD_LIBRARIES NAMES systemd ${PC_SYSTEMD_LIBRARY_DIRS})
-find_path(SYSTEMD_INCLUDE_DIRS systemd/sd-login.h HINTS ${PC_SYSTEMD_INCLUDE_DIRS})
+  find_package(PkgConfig QUIET)
+  pkg_check_modules(PC_SYSTEMD QUIET libsystemd)
+  find_library(SYSTEMD_LIBRARIES NAMES systemd ${PC_SYSTEMD_LIBRARY_DIRS})
+  find_path(SYSTEMD_INCLUDE_DIRS systemd/sd-login.h HINTS ${PC_SYSTEMD_INCLUDE_DIRS})
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(SYSTEMD DEFAULT_MSG SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
-mark_as_advanced(SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
+  include(FindPackageHandleStandardArgs)
+  find_package_handle_standard_args(SYSTEMD DEFAULT_MSG SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
+  mark_as_advanced(SYSTEMD_INCLUDE_DIRS SYSTEMD_LIBRARIES)
+
+  add_library(systemd::systemd SHARED IMPORTED)
+  set_target_properties(systemd::systemd PROPERTIES
+          INTERFACE_INCLUDE_DIRECTORIES  ${SYSTEMD_INCLUDE_DIRS}
+          IMPORTED_LOCATION              ${SYSTEMD_LIBRARIES}
+          IMPORTED_IMPLIB                ${SYSTEMD_LIBRARIES}
+          )
+endif()
