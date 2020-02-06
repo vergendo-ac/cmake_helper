fb662922931ca58fb1a3d0663412ac7a1ba9aa81
Andrew Penkrat <Andrey.Penkrat@lanit-tercom.com>
2020-02-06 19:28:49 +0300

    Don't pass -Wno-maybe-uninitialized to GCC 9+ and Clang
    GCC 9.x doesn't produce false positives with this warning.
    Clang simply doesn't have this flag.

--- EnableWarnings.cmake
+++ EnableWarnings.cmake
@@ -1,5 +1,8 @@
 function(enable_warnings TARGET)
     target_compile_options(${TARGET} PRIVATE
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
-        -Werror -Wall -Wextra -Wpedantic -Wnon-virtual-dtor -Wno-unused-function -Wno-maybe-uninitialized>)
+        -Werror -Wall -Wextra -Wpedantic -Wnon-virtual-dtor -Wno-unused-function>
+        # Disable maybe-uninitialized warning for GCC 8.x and earlier due to false positives
+        $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_LESS:${CMAKE_CXX_COMPILER_VERSION},9.0>>:
+        -Wno-maybe-uninitialized>)
 endfunction()
