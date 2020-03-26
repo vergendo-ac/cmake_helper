14b5ae7fad74087d447ac68fc2ec9571304bcb3b
Andrew Penkrat <Andrey.Penkrat@lanit-tercom.com>
2020-03-26 16:49:59 +0300

    Disable maybe-uninitialized warning for all GCC versions
    This warning didn't give false positives for doors project with GCC 9.1
    and 9.2 but does again with 9.3.
    It seems it would be better to simply ignore it on all versions.

--- EnableWarnings.cmake
+++ EnableWarnings.cmake
@@ -2,7 +2,7 @@ function(enable_warnings TARGET)
     target_compile_options(${TARGET} PRIVATE
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
         -Werror -Wall -Wextra -Wpedantic -Wnon-virtual-dtor -Wno-unused-function>
-        # Disable maybe-uninitialized warning for GCC 8.x and earlier due to false positives
-        $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_LESS:${CMAKE_CXX_COMPILER_VERSION},9.0>>:
+        # Disable maybe-uninitialized warning for GCC due to false positives
+        $<$<CXX_COMPILER_ID:GNU>:
         -Wno-maybe-uninitialized>)
 endfunction()
