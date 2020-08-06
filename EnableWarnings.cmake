2fea3fe244e23cb8edd0fcfc0e153d30af70bf14
Andrew Penkrat <andrew.penkrat@vergendo.com>
2020-08-06 15:45:58 +0300

    Only set compilation flags for C++ compilers
    This allows mixing multiple languages in a single target.

--- EnableWarnings.cmake
+++ EnableWarnings.cmake
@@ -1,8 +1,9 @@
 function(enable_warnings TARGET)
     target_compile_options(${TARGET} PRIVATE
+      $<$<COMPILE_LANGUAGE:CXX>:
         $<$<OR:$<CXX_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:AppleClang>,$<CXX_COMPILER_ID:GNU>>:
         -Werror -Wall -Wextra -Wpedantic -Wnon-virtual-dtor -Wno-unused-function>
         # Disable maybe-uninitialized warning for GCC due to false positives
         $<$<CXX_COMPILER_ID:GNU>:
-        -Wno-maybe-uninitialized>)
+        -Wno-maybe-uninitialized>>)
 endfunction()
